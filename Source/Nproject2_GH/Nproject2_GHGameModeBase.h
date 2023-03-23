a4bfb54ec17ee6956413125a8604673d3488ea1d
georgeholt97 <george.holt@student.nua.ac.uk>
2023-03-23 22:08:12 +0000

    Initial commit 2
    ~George H

---
+++ Source/Nproject2_GH/Nproject2_GHGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Nproject2_GHGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NPROJECT2_GH_API ANproject2_GHGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
