45377a669a5abc3e8c1fa722cc77b65b096d15a5
georgeholt97 <george.holt@student.nua.ac.uk>
2023-03-23 22:10:48 +0000

    Revert "Initial commit 2"
    This reverts commit a4bfb54ec17ee6956413125a8604673d3488ea1d.

--- Source/Nproject2_GH/Nproject2_GH.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Nproject2_GH.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Nproject2_GH, "Nproject2_GH" );
